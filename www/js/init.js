6ae1a0bbc04f0b8500ada373cae18c17758ae98e
smapps <info@smapps.nl>
2015-11-19 11:51:53 +0100

    Update init.js

--- www/js/init.js
+++ www/js/init.js
@@ -9,31 +9,27 @@ $(document).bind("mobileinit", function()
 });
 //var gaPlugin;
    
-document.addEventListener("deviceready", function(){
-	
-	/*navigator.geolocation.getCurrentPosition(onsuccess3,onerror3);
-	function onsuccess3(position)
-	{
-		//oke
-	}
-	function onerror3(position)
-	{
-		notification("U dient GPS aan te zetten om deze APP te gebruiken", "GPS fout","Oke");
-		//goToURL('outoforder.html');
-	}	*/
-	
-	window.plugin.statusbarOverlay.hide();
-
-}, true);
+//document.addEventListener("deviceready", onDeviceReady(), true);
 
 
 function onDeviceReady(){
 
-	/*setTimeout(function() {
-	 navigator.splashscreen.hide();
-	}, 500);*/
-
+	init('off');
+	return navigator.geolocation.getCurrentPosition(initialize);
+	document.addEventListener("backbutton", onBackKeyDown, false);
+	window.plugin.statusbarOverlay.hide();
 };
+function onLoad()
+{
+	if(typeof navigator.device == "undefined")
+	{
+		document.addEventListener("deviceready", onDeviceReady, false);
+	}
+	else{
+		onDeviceReady();
+	}
+}
+
 
 
 
