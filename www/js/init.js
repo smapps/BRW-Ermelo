d9f7f8b97efc000ac78f3b36459151c08cce0112
smapps <info@smapps.nl>
2015-11-19 14:24:45 +0100

    Update init.js

--- www/js/init.js
+++ www/js/init.js
@@ -1,42 +1,44 @@
-$(document).bind("mobileinit", function()
-{
-	/*
-  $.extend(  $.mobile , {
-    autoInitializePage: false,
-    loadingMessage: false
-  });
-  */
-});
-//var gaPlugin;
-   
-//document.addEventListener("deviceready", onDeviceReady(), true);
-
-
-function onDeviceReady(){
-
-	init('off');
-	return navigator.geolocation.getCurrentPosition(initialize);
-	document.addEventListener("backbutton", onBackKeyDown, false);
-	window.plugin.statusbarOverlay.hide();
-};
-function onLoad()
-{
-	if(typeof navigator.device == "undefined")
-	{
-		document.addEventListener("deviceready", onDeviceReady, false);
-	}
-	else{
-		onDeviceReady();
-	}
-}
-
-
-
-
-
 /*
-document.addEventListener("backbutton", onBackKeyDown, false);
-function onBackKeyDown(e) {
-  e.preventDefault();
-}
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+    function onDeviceReady()
+	{
+		//document.addEventListener('deviceready', getReady, false);
+		//return navigator.geolocation.getCurrentPosition();
+		init('off');
+		window.plugin.statusbarOverlay.hide();
+	}	
+   
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    function getReady(){
+      if(typeof navigator.device == "undefined")
+	  {
+		   document.addEventListener('deviceready', onDeviceReady, false);
+
+	  }
+	  else
+	  {
+		  window.plugin.statusbarOverlay.hide();
+			init('off');
+	  }
+	}
+	
